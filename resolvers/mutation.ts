9e5ec36c2410de4de527a39b34c017fea44a093e
Ali <afalconi@alumnos.nebrija.es>
2024-01-19 15:30:15 +0100

    si

---
+++ resolvers/mutation.ts
@@ -0,0 +1,5 @@
+export const Mutation = {
+    holaMundo: () => {
+        return "Hola Mundo";
+    }
+};
---
+++ resolvers/query.ts
@@ -0,0 +1,5 @@
+export const Query = {
+    holaMundo: () => {
+        return "Hola Mundo";
+    }
+};
